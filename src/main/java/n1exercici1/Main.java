f7879ebafc2f9924e39fa58f274ac099b3430221
clareta16 <clara.pages16@gmail.com>
2024-05-30 11:21:05 +0200

    Initial commit

---
+++ src/main/java/n1exercici1/Main.java
@@ -0,0 +1,14 @@
+package n1exercici1;
+
+import static n1exercici1.CridarTreballador.cridarMetodeSouTrimestral;
+import static n1exercici1.CridarTreballador.tipusTreballadors;
+
+public class Main {
+    public static void main(String[] args) {
+        tipusTreballadors();
+        cridarMetodeSouTrimestral();
+
+    }
+}
+
+
---
+++ src/main/java/n1exercici2/Main.java
@@ -0,0 +1,12 @@
+package n1exercici2;
+
+import static n1exercici1.CridarTreballador.tipusTreballadors;
+
+public class Main {
+    public static void main(String[] args) {
+        tipusTreballadors();
+
+    }
+}
+
+
